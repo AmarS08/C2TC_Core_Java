ec2b6682d15f99cbd8da41df899c2692d003cfbc
AMAR S <AMAR S@192.168.1.44>
2023-02-04 14:49:19 +0530

    First Commit

---
+++ Ravana/src/day1/Firstclass.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class Firstclass {
+
+	public static void main(String[] args) {
+		int a;
+		a = 10;
+		int b = 10;
+		double c = 10.5;
+		char d = 'a';
+		System.out.println("Hi 1");
+		System.out.println("hi 2");
+		System.out.println("hi 3");
+		System.out.print("HI hello 1");
+		System.out.print("HI hello 2");
+		System.out.print("HI hello 3\n");
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+
+}
---
+++ Ravana/src/day2/Oop.java
@@ -0,0 +1,26 @@
+package day2;
+
+public class Oop {
+	void methodA(){
+		System.out.println("A");
+	}
+	void methodB(){
+		System.out.println("B");
+	}
+	void methodC(){
+		System.out.println("C");
+	}
+	void methodD(){
+		System.out.println("D");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Oop o=new Oop();
+		o.methodD();
+		o.methodB();
+		o.methodC();
+		o.methodA();
+	}
+
+}
